6f319f5de6a6fef43dae44fba45623e574c89df2
Katelyn Kasperowicz <katelynkasperowicz@gmail.com>
2023-07-31 10:46:10 -0700

    Add Redcarpet and display markdown files

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -16,9 +16,13 @@ class DocumentsController < ApplicationController
     document = Document.find_by_shared_id!(params[:shared_id])
 
     if document.is_markdown_file?
-      @markdown = document.file.download
+      file = document.file.download
+      markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true)
+
+      @content = markdown.render(file)
+      render layout: 'article'
     else
-      redirect_to url_for(document.file)
+      redirect_to url_for(document.file) and return
     end
   end
 
