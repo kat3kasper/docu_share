6679c8dcd804f45d557f4fd480a7999c374bb5e4
Katelyn Kasperowicz <katelynkasperowicz@gmail.com>
2023-08-01 11:59:03 -0700

    Remove recoverable while email is not setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :rememberable, :validatable
+  # TODO: reinstate :recoverable once mailer setup
 end
